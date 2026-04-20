110438959cae67ce1c4c0e4dbef0c2aa49cee3f5
Kartikay Dubey <kartikayd2003@gmail.com>
2026-04-20 22:25:52 +0530

    fix: q quits in search mode

--- ui/v1/common/keys.go
+++ ui/v1/common/keys.go
@@ -37,8 +37,8 @@ func NewAppKeyMap() AppKeyMap {
 			key.WithHelp("?", "toggle help"),
 		),
 		Quit: key.NewBinding(
-			key.WithKeys("q", "ctrl+c"),
-			key.WithHelp("q", "quit"),
+			key.WithKeys("ctrl+c"),
+			key.WithHelp("ctrl+c", "quit"),
 		),
 		CycleLibrary: key.NewBinding(
 			key.WithKeys("tab"),
